4593fafdb1930c7b4bededfa91ac4f1fc7e73fa1
CupOfJavaJenny <jenniferazer@gmail.com>
2024-05-01 00:41:36 -0400

    updating person controller

--- src/main/java/io/zipcoder/crudapp/controller/PersonController.java
+++ src/main/java/io/zipcoder/crudapp/controller/PersonController.java
@@ -3,6 +3,10 @@ package io.zipcoder.crudapp.controller;
 import io.zipcoder.crudapp.entity.Person;
 import io.zipcoder.crudapp.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,12 +18,15 @@ import java.util.List;
 
 public class PersonController {
     @Autowired
+    //Update your controller logic to use the PersonRepository
     private PersonRepository personRepository;
     //List<Person> getPersonList(), Person updatePerson(Person p), and void DeletePerson(int id)
-    public List<Person> getPersonList(){
-        return new ArrayList<>();
+    @GetMapping
+    public ResponseEntity<List<Person>> getPersonList(){
+        List<Person> people = (List<Person>) personRepository.findAll();
+        return new ResponseEntity<>(people, HttpStatus.OK);
     }
-
+    @PostMapping
     public Person createPerson(Person person) {
         return person;
     }
--- src/main/java/io/zipcoder/crudapp/entity/Person.java
+++ src/main/java/io/zipcoder/crudapp/entity/Person.java
@@ -1,15 +1,20 @@
 package io.zipcoder.crudapp.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 
+
 public class Person {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
     private String firstName;
     private String lastName;
-    private int id;
+
     public String getFirstName() {
         return firstName;
     }
